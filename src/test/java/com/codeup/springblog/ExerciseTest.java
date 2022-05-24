b5630f39b51c72647d8cf6d6f6e8d2b5feb02538
Samuel Arguello <sam.rene12@gmail.com>
2022-05-24 13:39:08 -0500

    fixed refactor error

--- src/test/java/com/codeup/springblog/ExerciseTest.java
+++ src/test/java/com/codeup/springblog/ExerciseTest.java
@@ -18,7 +18,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import javax.servlet.http.HttpSession;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -36,17 +35,12 @@ public class ExerciseTest {
     private PostRepository postDao;
     @Autowired
     private UserRepository userDao;
-
     @Autowired
     private PasswordEncoder pen;
-
     @Autowired
     private MockMvc mvc;
-
     private User testUser;
-
     private HttpSession session;
-
 
     private void setUpTestUser() {
         testUser = userDao.findByUsername("testUser");
@@ -57,7 +51,6 @@ public class ExerciseTest {
             testUser.setUsername("testUser");
             testUser = userDao.save(testUser);
         }
-
     }
 
     private void setUpSession() throws Exception {
@@ -68,22 +61,7 @@ public class ExerciseTest {
                 .andReturn()
                 .getRequest()
                 .getSession();
-
     }
-
-//
-//    Exercises
-//    Create at least four integration tests for your Spring blog application.
-//    We will be focused on CRUD functionality for posts so make sure that:
-//
-//    A list of posts can be shown by visiting: /posts.
-//    A post page can be shown by visiting: /posts/{id}.
-//    A post can be created by submitting all the required params through a HTTP POST request to /posts/create.
-//    A test user has a valid HttpSession active before saving the post.
-//    Bonus
-//    A post can be updated by submitting a HTTP POST request to /posts/{id}/edit.
-//    A post can be deleted by submitting a HTTP DELETE request to /posts/{id}/delete.
-
 
     @Before
     public void setUp() throws Exception {
@@ -97,7 +75,6 @@ public class ExerciseTest {
         assertNotNull(session);
     }
 
-    // test post creation
     @Test
     public void testPostCreation() throws Exception {
         mvc.perform(
@@ -105,7 +82,8 @@ public class ExerciseTest {
                                 .with(csrf())
                                 .session((MockHttpSession) session)
                                 .flashAttr("post", new Post("xxTestTitlexx", "body text"))
-                ).andExpect(status().is3xxRedirection())
+                ).andExpect(status()
+                        .is3xxRedirection())
                 .andDo(print());
 
         Post p = postDao.findFirstByTitle("xxTestTitlexx");
@@ -113,35 +91,12 @@ public class ExerciseTest {
 
     }
 
-    // test post show
-
     @Test
     public void testPostShow() throws Exception {
         Post post = postDao.findAll().get(0);
 
-        mvc.perform(
-                        get("/posts/" + post.getId())
-                ).andExpect(status().isOk())
-                .andExpect(content().string(
-                        containsString(
-                                post.getTitle()
-                        )))
-                .andExpect(content().string(
-                        containsString(
-                                post.getBody()
-                        )
-                ));
-
-    }
-
-    @Test
-    // test post index
-    public void testPostIndex() throws Exception {
-        Post post = postDao.findAll().get(0);
-
-        mvc.perform(
-                        get("/posts")
-                ).andExpect(status().isOk())
+        mvc.perform(get("/posts/" + post.getId()))
+                .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString(
                                 post.getTitle()
@@ -152,32 +107,12 @@ public class ExerciseTest {
                         )));
     }
 
-
-    // test post edit
     @Test
-    public void testPostEdit() throws Exception {
+    public void testPostIndex() throws Exception {
+        Post post = postDao.findAll().get(0);
 
-        // need an existing item to edit
-        Post post = new Post("New Post", "I am the body", testUser);
-
-        post = postDao.save(post);
-
-        // new values to change the current values to
-
-        post.setTitle("Updated Title!");
-        post.setBody("Updated Body!!!");
-
-        mvc.perform(post("/posts/edit")
-                .with(csrf())
-                .session((MockHttpSession) session)
-                .flashAttr("post", post)
-        ).andExpect(status().is3xxRedirection());
-
-
-        // test if the values have changed
-        mvc.perform(
-                        get("/posts/" + post.getId())
-                ).andExpect(status().isOk())
+        mvc.perform(get("/posts"))
+                .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString(
                                 post.getTitle()
@@ -185,40 +120,51 @@ public class ExerciseTest {
                 .andExpect(content().string(
                         containsString(
                                 post.getBody()
-                        )
-                ));
+                        )));
+    }
 
-        // delete the new post we created.
+    @Test
+    public void testPostEdit() throws Exception {
+        Post post = new Post("New Post", "I am the body", testUser);
+        post = postDao.save(post);
+
+        post.setTitle("Updated Title!");
+        post.setBody("Updated Body!!!");
+
+        mvc.perform(post("/posts/edit")
+                .with(csrf())
+                .session((MockHttpSession) session)
+                .flashAttr("post", post))
+                .andExpect(status()
+                        .is3xxRedirection());
+
+        mvc.perform(get("/posts/" + post.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString(
+                                post.getTitle()
+                        )))
+                .andExpect(content().string(
+                        containsString(
+                                post.getBody()
+                        )));
         postDao.deleteById(post.getId());
     }
 
-
-    // test post delete
     @Test
     public void testPostDelete() throws Exception {
-
-
-        // make a post to delete
-        // need an existing item to delete
         Post post = new Post("New Post", "I am the body", testUser);
-
-
-
         post = postDao.save(post);
-
         Long id = post.getId();
-//        System.out.println("post.toString() = " + post.toString());
 
-        // post delete:f test that the post doesent exist
         mvc.perform(
                 post("/posts/delete")
                         .with(csrf())
                         .session((MockHttpSession) session)
-                        .param("post_id", String.valueOf(id))
-        ).andExpect(status().is3xxRedirection());
+                        .param("post_id", String.valueOf(id)))
+                .andExpect(status()
+                        .is3xxRedirection());
 
-
-        assertEquals(postDao.existsById(post.getId()), false);
+        assertFalse(postDao.existsById(post.getId()));
     }
-
 }
